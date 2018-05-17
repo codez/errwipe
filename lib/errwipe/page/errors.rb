c9e1a450afa3aa4f103320c7bcca7c5c7e2a44db
Severin Räz <raez@puzzle.ch>
2018-05-17 15:15:04 +0200

    Follow pagination, retry on failed requests

--- lib/errwipe/page/errors.rb
+++ lib/errwipe/page/errors.rb
@@ -8,6 +8,7 @@ module Errwipe
     class Errors < Base
 
       ERROR_ROWS_SELECTOR = 'form .errs .unresolved'
+      NEXT_LINK_SELECTOR = 'form > nav > span.next > a'
       HEADING_SELECTOR = 'h1'
       DELETE_BUTTON_LABEL = 'Delete'
 
@@ -15,13 +16,33 @@ module Errwipe
         find(HEADING_SELECTOR).first.text
       end
 
+      def empty?
+        error_rows.empty?
+      end
+
       def delete_errors!
+        @errors_deleted = false
+
         return unless form
-        
+
         error_rows.each do |row|
-          row.check! if yield row.message
+          if yield row.message
+            @errors_deleted = true
+            row.check!
+          end
         end
-        submit_delete!
+        submit_delete! if @errors_deleted
+      end
+
+      def next_url
+        link = find(NEXT_LINK_SELECTOR).first
+        return nil if link.nil?
+
+        link.attr('href')
+      end
+
+      def errors_deleted?
+        @errors_deleted
       end
 
       private
--- lib/errwipe/wiper.rb
+++ lib/errwipe/wiper.rb
@@ -9,6 +9,8 @@ require_relative 'page/errors'
 module Errwipe
   class Wiper
 
+    DELETE_SLEEP_PERIOD=0.5 # Seconds
+
     attr_reader :config
 
     def initialize
@@ -43,10 +45,48 @@ module Errwipe
       end
     end
 
-    def delete_errors(app_url)
-      page = Page::Errors.new(agent, app_url)
+    def delete_errors(url, page_index=0)
+      page = Page::Errors.new(agent, url)
+      return if page.empty?
 
-      print "Checking #{page.app_label}... "
+      print "Checking #{page.app_label} (page #{page_index})... "
+
+      perform_delete(page)
+
+      if page.errors_deleted?
+        # Errbit hates us if we delete too fast
+        sleep DELETE_SLEEP_PERIOD
+
+        # Maybe matching errors from the next page moved here
+        retry_on_known_errors { delete_errors(url, page_index) }
+      else
+        next_url = page.next_url
+        return if next_url.nil? # End of paging
+
+        retry_on_known_errors { delete_errors(next_url, page_index + 1) }
+      end
+    end
+
+    def retry_on_known_errors(try=1, &block)
+      begin
+        block.call
+      rescue Mechanize::ResponseCodeError => e
+        puts "got #{e.class.name}, trying again... "
+          sleep 2
+
+        if try < 3
+          retry_on_known_errors(try + 1, &block)
+        else
+          puts "Still getting #{e.class.name} #{e.message} after retrying, I am going to stop."
+        end
+      rescue SystemExit
+        exit 1
+      rescue Exception => e
+        puts "Unknown error #{e.class.name} #{e.message}\n#{e.backtrace.join("\n")}"
+      end
+    end
+
+    def perform_delete(page)
       page.delete_errors! do |message|
         Array(config.errors).any? { |e| message =~ e }
       end
