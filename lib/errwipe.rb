67e5d69c194ce3d1e810427f02c052c54a818844
Matthias Viehweger <kronn@kronn.de>
2019-04-15 14:00:52 +0200

    Configuration from file (#4)
    Switch from ENV-vars to config-file

--- lib/errwipe.rb
+++ lib/errwipe.rb
@@ -1,10 +1,13 @@
-# encoding: utf-8
-
 require 'rubygems'
 require 'mechanize'
 
+require_relative 'errwipe/configuration_file'
 require_relative 'errwipe/credential_prompt'
 require_relative 'errwipe/wiper'
 
+# Errwipe helps to wipe errors from errbit-project
+#
+# Some errors are beyond our control and can be removed. Errwipe does this
+# faster.
 module Errwipe
 end
